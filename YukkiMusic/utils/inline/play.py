8f482a34cd7db29d1f20546c3ff5d3225bb9e1fd
I am Saga <100592571+sagaProjects@users.noreply.github.com>
2023-02-02 18:52:49 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -70,16 +70,13 @@ def stream_markup(_, videoid):
             InlineKeyboardButton(
                 text=_["S_B_9"], url="t.me/banned_007"
             ),
-
-
+        ]
+        
+        [
             InlineKeyboardButton(
                 text=_["S_B_7"], url="t.me/alxelsupport"
             ),
 
-        ]
-
-        
-        [ 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             ),
